3c7802394f6de0c4867d68ac11fc0eae53053c7e
Thoriq <109299352+Thoriq0@users.noreply.github.com>
2023-01-14 10:03:19 +0700

    Upload File Portfolio
    Code Program yang masih perlu perubahan

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get element
+let arrow = document.querySelector('#container-h .content object');
+
+//  evenlistener
+arrow.addEventListener("click", ()=>{
+    // alert("oke");
+    window.scrollTo(0, 1040)
+});
