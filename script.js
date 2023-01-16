884fa35e86355c843688bb17164f76cddd349db3
Thoriq <109299352+Thoriq0@users.noreply.github.com>
2023-01-16 14:03:51 -0800

    Menghapus Script.js
    Untuk menambahkan fitur baru

--- script.js
+++
@@ -1,8 +0,0 @@
-// Get element
-let arrow = document.querySelector('#container-h .content object');
-
-//  evenlistener
-arrow.addEventListener("click", ()=>{
-    // alert("oke");
-    window.scrollTo(0, 1040)
-});
