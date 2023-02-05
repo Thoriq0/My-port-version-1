e5a24191da5613be2a26ae31cded300dd79a0fbe
Thoriq <109299352+Thoriq0@users.noreply.github.com>
2023-02-05 21:07:11 +0700

    membuat style untuk handphone - tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./tailwind/*{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'pop':['poppins']
+      },
+      letterSpacing:{
+        'common':['0.35rem'],
+        'another':['0.5rem']
+      },
+      lineHeight:{
+        'f':['64px']
+      }
+    },
   },
   plugins: [],
 }
